d0cb9ee1c5041dff1a9c9189a9ec16f2ef0c9e42
kibongcoders <kkp02052@gmail.com>
2025-05-21 23:04:47 +0900

    StructuredConcurrency 기본적인 취소

---
+++ src/main/kotlin/cancel/StructuredConcurrency.kt
@@ -0,0 +1,59 @@
+package cancel
+
+import coroutine_context.AccessingContextInASuspendingFunction
+import kotlinx.coroutines.*
+
+class StructuredConcurrency {
+
+    suspend fun `Join and Cancel`(): Unit = coroutineScope {
+        val job = launch {
+            repeat(1_000) {
+                i ->
+                delay(200)
+                println("Printing $i")
+            }
+        }
+
+        delay(1100)
+        job.cancel()
+        job.join()
+        println("Cancelled successfully")
+    }
+
+
+    suspend fun `Not Join and Cancel`(): Unit = coroutineScope {
+        val job = launch {
+            repeat(1_000) {
+                    i ->
+                delay(100)
+                Thread.sleep(100)
+                println("Thread Name : ${Thread.currentThread()} Printing $i")
+            }
+        }
+
+        delay(1000)
+        job.cancel()
+        println("Thread Name : ${Thread.currentThread()} Cancelled successfully")
+    }
+
+    suspend fun `CancelAndJoin Function`(): Unit = coroutineScope {
+        val job = launch {
+            repeat(1_000) {
+                    i ->
+                delay(200)
+                println("Thread Name : ${Thread.currentThread()} Printing $i")
+            }
+        }
+
+        delay(1100)
+        job.cancelAndJoin()
+        println("Thread Name : ${Thread.currentThread()} Cancelled successfully")
+    }
+}
+
+fun main() = runBlocking {
+    //StructuredConcurrency().`Join and Cancel`()
+    //StructuredConcurrency().`Not Join and Cancel`()
+    StructuredConcurrency().`CancelAndJoin Function`()
+}
+
