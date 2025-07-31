dd7d66f2a14cfb49126ec3b57f79965dfb9487be
kibongcoders <kkp02052@gmail.com>
2025-07-31 21:58:37 +0900

    DayFive test02 코드 처리하기
    https://school.programmers.co.kr/learn/courses/30/lessons/181931/solution_groups?language=kotlin&type=my

--- src/main/kotlin/coding_test/basic/DayFive.kt
+++ src/main/kotlin/coding_test/basic/DayFive.kt
@@ -10,7 +10,7 @@ class DayFive {
         for (i in code.indices) {
             val codeChar = code[i]
             if (codeChar == '1')
-                if (mode==0) mode = 1 else mode = 0
+                if (mode == 0) mode = 1 else mode = 0
             else {
                 if (mode == 1 && i % 2 != 0) {
                     result += codeChar.toString()
@@ -21,6 +21,15 @@ class DayFive {
             }
         }
         return result.ifEmpty { "EMPTY" }
+    }
+
+    fun test02(a: Int, d: Int, included: BooleanArray): Int {
+        var result = 0
+        for (i in 0..included.size - 1) {
+            if (included[i]) result += a + (i*d)
+        }
+
+        return result
     }
 }
 
